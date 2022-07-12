f250a14862a44f34b5e30d478d24b0bf72d27243
zhxxltx <3136713504@qq.com>
2022-07-12 21:32:12 +0800

    t

--- springboot-rabbitmq-config-consumer/src/main/java/com/zhx/service/direct/DirectSMSConsumer.java
+++ springboot-rabbitmq-config-consumer/src/main/java/com/zhx/service/direct/DirectSMSConsumer.java
@@ -1,5 +1,6 @@
 package com.zhx.service.direct;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class DirectSMSConsumer {
 
     @RabbitHandler
-    public void receiveMessage(String message) {
+    public void receiveMessage(String message, Channel channel) {
         System.out.println("sms direct ---- 收到了订单信息是：->" + message);
     }
 }
